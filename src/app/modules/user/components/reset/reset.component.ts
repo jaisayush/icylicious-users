933c6471fe5f80a4ae235d3227f2133cef1afdba
MOHAMED SHEHAS V <mohamedshehas.v2@mindtree.com>
2020-12-21 22:25:50 +0530

    login added

--- src/app/modules/user/components/reset/reset.component.ts
+++ src/app/modules/user/components/reset/reset.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetComponent implements OnInit {
 
-  constructor() { }
+  get email(){
+    return this.resetForm.get('email')
+  }
+
+  get question(){
+    return this.resetForm.get('question')
+  }
+
+  get answer(){
+    return this.resetForm.get('answer');
+  }
+
+  constructor(private fb:FormBuilder,private router:Router) { }
+  resetForm = this.fb.group({
+    email: ['',[Validators.required,Validators.email]],
+    question: ['',[Validators.required]],
+    answer: ['',[Validators.required,Validators.minLength(3)]],
+  },{
+  })
 
   ngOnInit(): void {
+  }
+
+  public failed:boolean = false;
+
+  onChange(){
+    this.failed = false;
+  }
+
+  onSubmit(){
+    console.log(this.resetForm.value)
+    // this._registrationService.reset(this.resetForm.value)
+    //   .subscribe(
+    //     response => {
+    //       console.log(response)
+    //       if(response.msg === "invalid" || response.msg === "failed"){
+    //         this.resetForm.reset();
+    //         this.failed = true;
+    //       }
+    //       else{
+    //         this.router.navigate(['change',{email:response.email}],{skipLocationChange:true})
+    //         this.resetForm.reset();
+    //       }
+          
+    //     },
+    //     error => console.log("error!",error)
+    //   );
+    // this.resetForm.reset();
   }
 
 }
